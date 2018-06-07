8525c92b06f6abe7e4648c0f9ece880a9b9d5f9e
LailaAlSaadi <alsaadiilaila@gmail.com>
2018-06-07 23:03:43 +0300

    Design The UI for The App:In Progress

--- app/src/main/java/com/u689/timetableapp/MainActivity.java
+++ app/src/main/java/com/u689/timetableapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.u689.timetableapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById
+    Spinner citySpinner;
+
+
+    @ViewById
+    Spinner routeSpinner;
+
+    ArrayAdapter<String> routsAdapter;
+
+
+    @Background
+    public void citySpinner(View view) {
+        String[] routes = this.getResources().getStringArray(R.array.routes_1);
+        routsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, routes);
+        citySpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                routeSpinner.setAdapter(routsAdapter);
+            }
+        });
     }
 }
