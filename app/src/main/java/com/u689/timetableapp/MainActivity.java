586e10eb95f3c04ace2a89df65d8cf93a615b73c
LailaAlSaadi <alsaadiilaila@gmail.com>
2018-06-08 17:39:29 +0300

    UI design for App: in progress

--- app/src/main/java/com/u689/timetableapp/MainActivity.java
+++ app/src/main/java/com/u689/timetableapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.u689.timetableapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import static android.R.layout.simple_spinner_item;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -18,22 +25,86 @@ public class MainActivity extends AppCompatActivity {
     @ViewById
     Spinner citySpinner;
 
-
     @ViewById
     Spinner routeSpinner;
 
-    ArrayAdapter<String> routsAdapter;
+    @ViewById
+    TextView textTimer;
+
+    @ViewById (R.id.startTimer)
+    TextView startTimerBtn;
+
+    @ViewById (R.id.stopTimer)
+    TextView stopTimerBtn;
 
 
-    @Background
-    public void citySpinner(View view) {
-        String[] routes = this.getResources().getStringArray(R.array.routes_1);
-        routsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, routes);
-        citySpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+    ArrayAdapter<CharSequence> adapter;
+    private Handler myHandler = new Handler();
+
+    @AfterViews
+    public void onChangeCitySpinner() {
+
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                routeSpinner.setAdapter(routsAdapter);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String s = "routes_" + position;
+                int identifier = getResources().getIdentifier(s, "array", getPackageName());
+                adapter = ArrayAdapter.createFromResource(getApplicationContext(), identifier, simple_spinner_item);
+                routeSpinner.setAdapter(adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
+
+    private long startTime;
+    private long timeInMillies;
+    private long finalTime;
+    private long timeSwap;
+
+    @Click
+    public void startTimer() {
+        stopTimerBtn.setEnabled(true);
+        startTimerBtn.setEnabled(false);
+        citySpinner.setEnabled(false);
+        routeSpinner.setEnabled(false);
+        startTime = SystemClock.uptimeMillis();
+        myHandler.postDelayed(updateTimerMethod, 0);
+    }
+
+    @Click
+    public void stopTimer() {
+        citySpinner.setEnabled(true);
+        routeSpinner.setEnabled(true);
+        stopTimerBtn.setEnabled(false);
+        startTimerBtn.setEnabled(true);
+        finalTime = 0;
+        myHandler.removeCallbacks(updateTimerMethod);
+    }
+
+    private Runnable updateTimerMethod = new Runnable() {
+        public void run() {
+            timeInMillies = SystemClock.uptimeMillis() - startTime;
+            finalTime = timeSwap + timeInMillies;
+
+            int seconds = (int) (finalTime / 1000);
+            int minutes = seconds / 60;
+            int hours = minutes / 60;
+            seconds = seconds % 60;
+            textTimer.setText(getFormatedTime(seconds, minutes, hours));
+            myHandler.postDelayed(this, 0);
+        }
+
+    };
+
+    @NonNull
+    private String getFormatedTime(int seconds, int minutes, int hours) {
+        return String.format("%02d", hours) + ":" + String.format("%02d", minutes) + ":"
+                + String.format("%02d", seconds);
+    }
+
 }
